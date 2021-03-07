604ad4072d2ecd2633248f3a6c23c6e8228b9261
Daria Burdyga <Daria.Burdyga@t-systems.com>
2021-03-07 23:35:10 +0300

    added Context

---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,13 @@
+import React, {useContext} from "react";
+import PropTypes from 'prop-types'
+import Context from "../context";
+
+function AddTodo() {
+  return (
+    <form>
+      
+    </form>
+  )
+}
+
+export default AddTodo

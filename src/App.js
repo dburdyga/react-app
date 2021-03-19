afd205e1e80f64b74ec7511901fe31e04ef9e6ef
Daria Burdyga <Daria.Burdyga@t-systems.com>
2021-03-19 19:26:46 +0300

    added Loader

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, {useEffect} from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
-
-//
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   .then(response => response.json())
-//   .then(json => console.log(json))
+import Loader from "./Loader";
 
 function App() {
-  const [todos, setTodos] = React.useState(
-    [
-      {id: 1, completed: false, title: 'Buy bread'},
-      {id: 2, completed: false, title: 'Buy milk'},
-      {id: 3, completed: false, title: 'Buy eggs'}
-    ]
-  )
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState([true])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(response => response.json())
+    .then(todos =>{
+      setTimeout(() => {
+        setTodos(todos)
+        setLoading(false)
+      }, 2000)
+    })
+  }, [])
 
   function toggleTodo(id) {
     setTodos(
@@ -45,7 +47,13 @@ function App() {
       <div className='wrapper'>
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo}/>
-        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos!</p>}
+
+        {loading && <Loader/>}
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : (
+          loading ? null : (
+            <p>No todos!</p>
+          )
+        )}
       </div>
     </Context.Provider>
   );
